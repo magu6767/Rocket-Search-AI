cd7b26054d7e6c2280d2f2e516ecc9a201a8da04
Hideto Maguchi <hidetomaguchi@gmail.com>
2025-02-16 00:18:47 +0900

    権限・ログイン回りの雛形作成

--- frontend/src/background.ts
+++ frontend/src/background.ts
@@ -1,1 +1,38 @@
-console.log('backgroundです！！！');
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithCredential, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBD03JuDxiNNz-yZGi8CJrTjj2LiCR6FUU",
+  authDomain: "web-extention.firebaseapp.com",
+  projectId: "web-extention",
+  storageBucket: "web-extention.firebasestorage.app",
+  messagingSenderId: "503808106062",
+  appId: "1:503808106062:web:349c320b47f90a181fed1f"
+};
+
+initializeApp(firebaseConfig);
+
+console.log('background.ts');
+
+const signIn = async (sendResponse:any) => {
+  const token = await new Promise<string>((resolve) => {
+    chrome.identity.getAuthToken({ 'interactive': true }, (token) => {
+      if (token) resolve(token);
+    });
+  });
+
+  const auth = getAuth();
+  const credential = GoogleAuthProvider.credential(null, token);
+
+  const userCredential = await signInWithCredential(auth, credential);
+
+  console.log(userCredential);
+  sendResponse({ success: true });
+};
+
+chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
+  if (request.action === 'signIn') {
+    signIn(sendResponse);
+    return true;
+  }
+});
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { crx, defineManifest } from '@crxjs/vite-plugin';
+import dotenv from 'dotenv';
+
+// .envファイルを読み込む
+dotenv.config();
 
 const manifest = defineManifest({
   manifest_version: 3,
@@ -19,6 +23,20 @@ const manifest = defineManifest({
   background: {
     service_worker: 'src/background.ts',
   },
+  permissions: [
+    'identity',
+  ],
+  content_security_policy: {
+    extension_pages: "script-src 'self'; object-src 'self'",
+    sandbox: "sandbox allow-scripts; script-src 'self' https://apis.google.com https://www.gstatic.com https://www.googleapis.com https://securetoken.googleapis.com; object-src 'self'",
+  },
+  oauth2: { 
+    // Google CloudのOAuth 2.0 クライアント ID
+    client_id: process.env.GOOGLE_CLIENT_ID ?? '',
+    scopes: ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile'],
+  },
+  // Chromeウェブストアの公開キー
+  key: process.env.EXTENSION_PUBLIC_KEY,
 });
 
 // https://vite.dev/config/
