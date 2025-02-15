847beb861f076e83346df041ba7ec71963fb2058
Hideto Maguchi <hidetomaguchi@gmail.com>
2025-02-15 20:14:28 +0900

    frontendディレクトリを作成

---
+++ frontend/vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { crx, defineManifest } from '@crxjs/vite-plugin';
+
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: 'Chrome拡張機能の練習',
+  version: '1.0.0',
+  description: 'Zenn投稿するChrome拡張機能のサンプルです。',
+  action: {
+    default_popup: 'index.html',
+  },
+});
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), crx({ manifest })],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173,
+    },
+  },
+})
