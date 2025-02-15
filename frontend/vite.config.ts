59b03f073e9d08efbd9d30ff021bca113ff64f1e
Hideto Maguchi <hidetomaguchi@gmail.com>
2025-02-15 22:48:48 +0900

    content.tsの作成

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,8 +8,14 @@ const manifest = defineManifest({
   version: '1.0.0',
   description: 'Zenn投稿するChrome拡張機能のサンプルです。',
   action: {
-    default_popup: 'index.html',
+    default_popup: 'popup.html',
   },
+  content_scripts: [
+    {
+      matches: ['<all_urls>'],
+      js: ['src/content.ts'],
+    },
+  ],
 });
 
 // https://vite.dev/config/
