406611c058fa6fbd03dd4e14578f2fe0ab93077c
Hideto Maguchi <hidetomaguchi@gmail.com>
2025-03-30 18:48:24 +0900

    フロントエンドのバージョン番号を1.0.1に更新

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ dotenv.config();
 const manifest = defineManifest({
   manifest_version: 3,
   name: '__MSG_appName__',
-  version: '1.0.0',
+  version: '1.0.1',
   description: '__MSG_appDescription__',
   default_locale: 'en',
   icons: {
