042bfcd1f4bb4a2d45350922b659862ec1612493
Hideto Maguchi <hidetomaguchi@gmail.com>
2025-08-11 18:31:32 +0900

    アプリのバージョンを変更

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ dotenv.config();
 const manifest = defineManifest({
   manifest_version: 3,
   name: '__MSG_appName__',
-  version: '1.0.3',
+  version: '1.0.4',
   description: '__MSG_appDescription__',
   default_locale: 'en',
   icons: {
